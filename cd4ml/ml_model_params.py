9abbbb271febea015d5d21256d6312d6ba058445
Geoffroy GOBERT <geoffroy.gobert@tier.app>
2020-07-30 15:37:56 +0200

    Make the deployment pipeline failing

--- cd4ml/ml_model_params.py
+++ cd4ml/ml_model_params.py
@@ -1,13 +1,13 @@
 model_parameters = {
     "random_forest": {
-        "n_estimators": 100,
+        "n_estimators": 5,
         "max_features": 0.5
     },
     "adaboost": {
-        "n_estimators": 100
+        "n_estimators": 5
     },
     "gradient_boosting": {
-        "n_estimators": 200,
+        "n_estimators": 5,
         "max_depth": 4
     },
     "decision_tree": {
--- cd4ml/pipeline_params.py
+++ cd4ml/pipeline_params.py
@@ -6,7 +6,7 @@ from cd4ml.ml_model_params import model_parameters
 pipeline_params = {'model_name': 'random_forest',
                    'days_back': 57,
                    'acceptance_metric': 'r2_score',
-                   'acceptance_threshold_min': 0.60,
+                   'acceptance_threshold_min': 0.80,
                    'acceptance_threshold_max': 1.0,
                    'data_source': 'file',
                    'model_params': model_parameters,
